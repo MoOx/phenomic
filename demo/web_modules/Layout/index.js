25c14df218bbacfab3ee89473474e8b0e8e0e57e
Maxime Thirouin <m@moox.io>
2015-09-16 09:06:50 +0200

    Add Google Analytics

--- demo/web_modules/Layout/index.js
+++ demo/web_modules/Layout/index.js
@@ -4,11 +4,35 @@ import { PropTypes } from "react"
 import { Link } from "react-router"
 
 import styles from "./index.css"
+import pkg from "../../package.json"
+
+import ga from "react-google-analytics"
+const GoogleAnalyticsInitiailizer = ga.Initializer
 
 export default class Layout extends Component {
 
   static propTypes = {
     children: PropTypes.oneOfType([ PropTypes.array, PropTypes.object ]),
+    params: PropTypes.object,
+  }
+
+  componentWillMount() {
+    ga("create", pkg.statinamic.googleAnalyticsUA, "auto")
+    this.logPageview()
+
+  }
+
+  componentWillReceiveProps(props) {
+    if (props.params.splat !== this.props.params.splat) {
+      this.logPageview()
+    }
+  }
+
+  logPageview() {
+    ga("send", "pageview")
+    if (__DEV__) {
+      console.info("New pageview", window.location.href)
+    }
   }
 
   render() {
@@ -26,6 +50,7 @@ export default class Layout extends Component {
           {"© 2015 - "}
           <a href="http://moox.io/">@MoOx</a>
         </footer>
+        <GoogleAnalyticsInitiailizer />
       </div>
     )
   }
