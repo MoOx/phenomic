711ae9bdeaffff5d4ec614f9ed8f1148b9eab526
Khoa Nguyen <khoa02031995@gmail.com>
2016-01-27 01:22:22 +0700

    proof of concept for load data via Webpack

---
+++ src/redux/modules/loadPage.js
@@ -0,0 +1,12 @@
+export default function reducer(state = {} /* , action */) {
+  // switch (action.type) {
+  // case "LAYOUTS_SET":
+  //   return {
+  //     ...action.layouts,
+  //   }
+  //
+  // default:
+  //   return state
+  // }
+  return state
+}
