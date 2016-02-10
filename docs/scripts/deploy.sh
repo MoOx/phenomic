502adbf756a97c2ed15af1f42f7d31ac55d399ff
Maxime Thirouin <m@moox.io>
2016-02-10 23:22:54 +0100

    Fix autodeploy for doc
    Closes #123

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -4,5 +4,6 @@ git config --global user.email "gh-pages@localhost"
 git config --global user.name "npm gh-pages"
 
 ./node_modules/.bin/gh-pages \
+  --silent \
   --repo https://$GITHUB_TOKEN@github.com/MoOx/statinamic.git \
   --dist docs/dist
