b609518249ea58f89358e6b8214a494f8a4688d4
Maxime Thirouin <m@moox.io>
2016-02-10 08:52:20 +0100

    Add an author for deploy
    https://travis-ci.org/MoOx/statinamic/jobs/108218013#L2127

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git config --global user.email "gh-pages@localhost"
+git config --global user.name "npm gh-pages"
+
 ./node_modules/.bin/gh-pages \
   --repo https://$GITHUB_TOKEN@github.com/MoOx/statinamic.git \
   --dist docs/dist
