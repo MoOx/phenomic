4b50c5630d146d9ede6614664b9444718183202d
Maxime Thirouin <m@moox.io>
2016-02-18 08:23:48 +0100

    Move exports back to the build script
    Closes #162

--- boilerplate/scripts/build.js
+++ boilerplate/scripts/build.js
@@ -1,11 +1,15 @@
 import builder from "statinamic/lib/builder"
 
-import * as exports from "../web_modules/app/exports"
-import store from "../web_modules/app/store"
-
 import config from "./config.js"
 import webpackConfig from "./webpack.config.babel.js"
 import clientWebpackConfig from "./webpack.config.client.js"
+
+const exports = {
+  layouts: require.resolve("../web_modules/layouts"),
+  metadata: require.resolve("../web_modules/app/metadata"),
+  routes: require.resolve("../web_modules/app/routes"),
+}
+import store from "../web_modules/app/store"
 
 builder({
   config,
--- docs/web_modules/app/exports.js
+++
@@ -1,3 +0,0 @@
-export const layouts = require.resolve("../layouts")
-export const metadata = require.resolve("./metadata")
-export const routes = require.resolve("./routes")
