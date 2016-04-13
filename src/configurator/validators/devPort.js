03809925c6b222a5311c94abe5cf8041c12f7e8c
Khoa Nguyen <hi@khoanguyen.me>
2016-04-14 02:45:42 +0700

    Fix wrong if statement

--- src/configurator/validators/devPort.js
+++ src/configurator/validators/devPort.js
@@ -4,12 +4,12 @@ export default function(
 ): void {
   const port = Math.trunc(config.devPort)
 
-  if (!port > 0) {
+  if (port > 0) {
+    config.devPort = port
+  }
+  else {
     errors.push(
       "devPort can be a string or a number and must be a legal port"
     )
-  }
-  else {
-    config.devPort = port
   }
 }
