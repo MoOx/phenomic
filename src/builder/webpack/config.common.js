745669d47ec448246c89a3d751245623dc7b2d9d
Maxime Thirouin <m@moox.io>
2016-04-29 07:26:52 +0200

    Fixed: PHENOMIC_NAME now have the pretty name, not the version

--- src/builder/webpack/config.common.js
+++ src/builder/webpack/config.common.js
@@ -23,7 +23,7 @@ export default (config) => {
 
         PHENOMIC_USER_PATHNAME: wrap(process.env.PHENOMIC_USER_PATHNAME),
         PHENOMIC_USER_URL: wrap(url.format(config.baseUrl)),
-        PHENOMIC_NAME: wrap(pkg.version),
+        PHENOMIC_NAME: wrap(pkg.name[0].toUpperCase() + pkg.name.slice(1)),
         PHENOMIC_VERSION: wrap(pkg.version),
         PHENOMIC_HOMEPAGE: wrap(pkg.homepage),
         PHENOMIC_REPOSITORY: wrap(pkg.repository),
