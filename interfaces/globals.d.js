2322d300e5af1ab5defafb7880db9dd28402f389
Maxime Thirouin <m@moox.io>
2016-04-29 07:22:11 +0200

    Internal: fix process.env possible values for flow

--- interfaces/globals.d.js
+++ interfaces/globals.d.js
@@ -3,7 +3,12 @@ declare function cwd(): string;
 declare var process: {
   env: {
     NODE_ENV? : "production",
+    PHENOMIC_USER_URL: string,
     PHENOMIC_USER_PATHNAME: string,
+    PHENOMIC_NAME: string,
+    PHENOMIC_VERSION: string,
+    PHENOMIC_HOMEPAGE: string,
+    PHENOMIC_REPOSITORY: string,
   },
   cwd: cwd,
   agrv: Array<string>,
