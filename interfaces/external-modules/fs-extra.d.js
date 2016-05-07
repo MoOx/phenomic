7d54aa943423308baafbec0a2b587fc9413efa18
Khoa Nguyen <hi@khoanguyen.me>
2016-05-07 15:18:08 +0700

    Add flow tpe for fs.removeSync

--- interfaces/external-modules/fs-extra.d.js
+++ interfaces/external-modules/fs-extra.d.js
@@ -1,10 +1,12 @@
 declare module "fs-extra" {
   declare function emptyDirSync(from: string, to: string): void;
   declare function copySync(from: string, to: string): void;
+  declare function removeSync(target: string): void;
 
   declare interface fsExtra {
     emptyDirSync: emptyDirSync,
     copySync: copySync,
+    removeSync: removeSync,
   }
   declare var exports: fsExtra;
 }
