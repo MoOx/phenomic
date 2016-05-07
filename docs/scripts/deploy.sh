b36d088b748ca4cffdbd5a58e78c09df9f3c6953
Maxime Thirouin <m@moox.io>
2016-05-07 07:57:51 +0200

    Internal: simplify docs deploy script

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
+GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
 
-if [ "$TRAVIS" = "true" ]
-then
-  git config --global user.email "gh-pages@localhost"
-  git config --global user.name "npm gh-pages"
-fi
-
-./node_modules/.bin/gh-pages \
-  --silent \
-  --repo https://$GITHUB_TOKEN@github.com/MoOx/phenomic.git \
-  --dist docs/dist
+cd dist && \
+$(npm bin)/rimraf .git
+git init && \
+git add . && \
+git commit -m "Deploy to GitHub Pages" && \
+git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
