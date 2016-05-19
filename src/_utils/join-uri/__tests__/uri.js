9e5c21407662a222ae0cf362248fda5e1b1c7407
Ezra Lalonde <ezra@usefuliftrue.com>
2016-05-19 16:21:28 -0600

    + Added `url-join` package to project + Replaced implementation of `join-uri` with call to `url-join` + Added a few tests in uri.js + Broke tests that depend on old behaviour

---
+++ src/_utils/join-uri/__tests__/uri.js
@@ -0,0 +1,64 @@
+import test from "ava"
+
+import { sep } from "path"
+
+import joinUri from ".."
+
+// Tests for URIs
+test("should preserve protocol-relative uri", (t) => (
+  t.is(joinUri("//some.site", "page"),
+  "//some.site/page")
+))
+
+test("should preserve proto with slashed arg", (t) => (
+  t.is(joinUri("//some.site/", "///page/thing/"),
+  "//some.site/page/thing/")
+))
+
+test("should preserve schema", (t) => (
+  t.is(joinUri("https://some.site", "page"),
+  "https://some.site/page")
+))
+
+test("should preserve trailing slash", (t) => (
+  t.is(joinUri("https://some.site", "page/"),
+  "https://some.site/page/")
+))
+
+test("should remove duplicate slash after TLD", (t) => (
+  t.is(joinUri("https://some.site/", "/page/"),
+  "https://some.site/page/")
+))
+
+test("should remove duplicate slashes", (t) => (
+  t.is(joinUri("https://some.site/", "/page/", "/index.html"),
+  "https://some.site/page/index.html")
+))
+
+test("should join query param to root", (t) => (
+  t.is(
+    joinUri("https://some.site/", "?query=text"),
+    "https://some.site?query=text"
+  )
+))
+
+test("should join query param", (t) => (
+  t.is(
+    joinUri("https://some.site", "index.html", "?query=text"),
+    "https://some.site/index.html?query=text"
+  )
+))
+
+test("should join relative URI parts", (t) => (
+  t.is(
+    joinUri("some", "thing"),
+    "some/thing"
+  )
+))
+
+test("should join multipe relative URI parts", (t) => (
+  t.is(
+    joinUri("some", "thing", "else"),
+    "some/thing/else"
+  )
+))
