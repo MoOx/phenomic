MoOx/phenomic

commit 70b465391b76f9bba920207de1b0eab86fef1f84

Fixed: public `joinUri` function now correctly join uri with protocol
- Internal: `join-uri` now `path-to-uri`.
  [#500](https://github.com/MoOx/phenomic/pull/500)

Closes #489

src/_utils/path-to-uri/index.js
@@ -1,0 +1,10 @@
+// @flow
+import { posix } from "path"
+
+const pathToUri = function(...args: Array<string>): string {
+  return posix.normalize(
+           posix.join(...args).replace(/\\/g, "/")
+         )
+}
+
+export default pathToUri

src/_utils/html-metas/index.js
@@ -1,5 +1,5 @@
 // @flow
-import joinUri from "../join-uri"
+import pathToUri from "../path-to-uri"
 
 export const defaultMetas = [
   "<meta charset=\"utf-8\" />",
@@ -14,7 +14,7 @@ export default function(
 
   if (css && Array.isArray(css)) {
     css.forEach(fileName => {
-      const path = joinUri(baseUrl.pathname, fileName)
+      const path = pathToUri(baseUrl.pathname, fileName)
       metas.push(
         `<link rel="stylesheet" href="${path}" />`
       )

src/_utils/join-uri/index.js
@@ -1,12 +1,0 @@
-// @flow
-import { join, sep } from "path"
-
-const joinUri = function(...args: Array<string>): string {
-  return sep === "/"
-  // unix
-  ? join(...args)
-  // windows require replacement of \ to /
-  : join(...args).replace(/\\/g, "/")
-}
-
-export default joinUri

src/_utils/offline/appcache/index.js
@@ -3,7 +3,7 @@ import globby from "../globby"
 import { join } from "path"
 import { writeFile } from "fs-promise"
 import template from "./template"
-import joinUri from "../../join-uri"
+import pathToUri from "../../path-to-uri"
 
 const writeAppcache = function(
   distPath: string,
@@ -11,10 +11,10 @@ const writeAppcache = function(
   pattern: Array<string>
 ): Promise {
   const destination = join(distPath, "manifest.appcache")
-  const fallback = joinUri("/", baseUrl, "/")
+  const fallback = pathToUri("/", baseUrl, "/")
 
   return globby(pattern, distPath)
-    .then((files) => files.map((file) => joinUri("/", baseUrl, file)))
+    .then((files) => files.map((file) => pathToUri("/", baseUrl, file)))
     .then((files) => {
       writeFile(destination, template(files, fallback))
     })

src/_utils/offline/service-worker/index.js
@@ -2,7 +2,7 @@
 import { writeFile } from "fs-promise"
 import { join } from "path"
 import globby from "../globby"
-import joinUri from "../../join-uri"
+import pathToUri from "../../path-to-uri"
 import compiler from "./compile-template"
 
 const writeSwRegister = function(
@@ -26,7 +26,7 @@ const writeSw = function(
   pattern: Array<string>
 ): Promise {
   return globby(pattern, distPath)
-  .then((files) => files.map((filename) => joinUri(scope, "/", filename)))
+  .then((files) => files.map((filename) => pathToUri(scope, "/", filename)))
   .then((files) => (
     compiler(
       join(__dirname, "sw.template.js"),
@@ -47,7 +47,7 @@ export default function(
   baseUrl: string,
   pattern: Array<string>
 ): Promise {
-  const scope = joinUri("/", baseUrl, "/")
+  const scope = pathToUri("/", baseUrl, "/")
   return Promise.all([
     writeSwRegister(scope, distPath),
     writeSw(scope, distPath, pattern),

src/_utils/path-to-uri/__tests__/index.js
@@ -2,39 +2,53 @@ import test from "ava"
 
 import { sep } from "path"
 
-import joinUri from ".."
+import pathToUri from ".."
 
 test("should join 2 uri parts", (t) => (
   t.is(
-    joinUri("some", "thing"),
+    pathToUri("some", "thing"),
     "some/thing"
+  )
+))
+
+test("should fix mixed paths", (t) => (
+  t.is(
+    pathToUri("//some\\", "\\\\thing\\"),
+    "/some/thing/"
   )
 ))
 
 test("should join 2 uri parts with a slash", (t) => (
   t.is(
-    joinUri("some", "/thing"),
+    pathToUri("some", "/thing"),
     "some/thing"
   )
 ))
 
 test("should join 2 uri parts with slashes", (t) => (
   t.is(
-    joinUri("some/", "/thing"),
+    pathToUri("some/", "/thing"),
     "some/thing"
   )
 ))
 
 test("should join multiple uri parts", (t) => (
   t.is(
-    joinUri("some", "thing", "else"),
+    pathToUri("some", "thing", "else"),
     "some/thing/else"
+  )
+))
+
+test("eliminates leading/duplicate slashes", (t) => (
+  t.is(
+    pathToUri("//one/", "/more/", "//time//"),
+    "/one/more/time/"
   )
 ))
 
 test("should join and fix windows shit (on windows :trollface:)", (t) => (
   t.is(
-    joinUri("some", "thing", `else${ sep }from${ sep }windows`),
+    pathToUri("some", "thing", `else${ sep }from${ sep }windows`),
     "some/thing/else/from/windows"
   )
 ))

src/_utils/urlify/index.js
@@ -1,5 +1,5 @@
 // @flow
-import joinUri from "../join-uri"
+import pathToUri from "../path-to-uri"
 
 export const fileExtensionRE = /\.html?$/
 
@@ -26,7 +26,7 @@ export default function urlify(
   if (!hasExtension) {
     if (full) {
       // url without extension => folder => index.html
-      url = joinUri(url, "index.html")
+      url = pathToUri(url, "index.html")
     }
     else {
       // url without extension => folder

src/content-loader/index.js
@@ -3,7 +3,7 @@ import loaderUtils from "loader-utils"
 import frontMatterParser from "gray-matter"
 import colors from "chalk"
 
-import joinUri from "../_utils/join-uri"
+import pathToUri from "../_utils/path-to-uri"
 import urlify from "../_utils/urlify"
 import enhanceCollection from "../enhance-collection"
 import feed from "./feed"
@@ -69,9 +69,9 @@ module.exports = function(input) {
 
   const metadata = {
     __filename: relativePath,
-    __url: joinUri("/", url),
-    __resourceUrl: joinUri("/", resourceUrl),
-    __dataUrl: joinUri("/", dataUrl),
+    __url: pathToUri("/", url),
+    __resourceUrl: pathToUri("/", resourceUrl),
+    __dataUrl: pathToUri("/", dataUrl),
   }
   let textData = {
     head: {
@@ -137,5 +137,5 @@ module.exports = function(input) {
     }, 100)
   }
 
-  return "module.exports = " + JSON.stringify(joinUri("/", dataUrl))
+  return "module.exports = " + JSON.stringify(pathToUri("/", dataUrl))
 }

commit c1f59a3c8ffd9897c6557db3aa299e749db9d270

Internal: Fix regressionwith path-to-uri introduced in #500

src/_utils/path-to-uri/index.js
@@ -1,10 +1,12 @@
 // @flow
-import { posix } from "path"
+import { join } from "path"
 
+/**
+ * Normalize path into uri
+ * Join, replace multiple / or \ to single /
+ */
 const pathToUri = function(...args: Array<string>): string {
-  return posix.normalize(
-           posix.join(...args).replace(/\\/g, "/")
-         )
+  return join(...args).replace(/(\/|\\)+/g, "/")
 }
 
 export default pathToUri