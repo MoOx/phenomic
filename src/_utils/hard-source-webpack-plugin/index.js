69f3028c3b46e5dbd232a1e713780989f58bcaa7
Khoa Nguyen <hi@khoanguyen.me>
2016-09-01 05:13:40 +0700

    Add a simple hash to determine when does the hash changed

--- src/_utils/hard-source-webpack-plugin/index.js
+++ src/_utils/hard-source-webpack-plugin/index.js
@@ -2,12 +2,14 @@
 import { join } from "path"
 import findCacheDir from "find-cache-dir"
 import HardSourceWebpackPlugin from "hard-source-webpack-plugin"
+import ObjectHash from "node-object-hash"
 
 export const getCacheDir = (config: PhenomicConfig): string => {
   // Calculate hash based on config object
-  console.log(config)
-  const env = (config.production) ? "prod" : "dev"
-  const cacheDir = findCacheDir({ name: "phenomic-hard-source-wp/" + env })
+  const hasher = new ObjectHash()
+  const hash = hasher.hash(config)
+
+  const cacheDir = findCacheDir({ name: "phenomic-hard-source-wp/" + hash })
 
   return cacheDir
 }
