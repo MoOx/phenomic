e3b3d1bff812c9d2f2cc3a0ecea70b3463eb84ea
Khoa Nguyen <hi@khoanguyen.me>
2016-09-01 13:12:48 +0700

    Cache static build too

--- src/builder/webpack/config.browser.js
+++ src/builder/webpack/config.browser.js
@@ -3,26 +3,18 @@
 import { join } from "path"
 import commonWebpackConfig from "./config.common.js"
 import { offlinePlugin, offlineEntry } from "../../_utils/offline/webpack.js"
-import {
-  getCacheDir,
-  hardSourceRecordsPath,
-  hardSourcePlugin,
-} from "../../_utils/hard-source-webpack-plugin"
 
 const chunkNameBrowser = "phenomic.browser"
 
 export default (config: PhenomicConfig): WebpackConfig => {
 
-  const cacheDir = getCacheDir(config)
   const webpackConfig = commonWebpackConfig(config)
 
   return {
     ...webpackConfig,
-    ...config.webpackHardCache && hardSourceRecordsPath(cacheDir),
     plugins: [
       ...webpackConfig.plugins,
       ...offlinePlugin(config),
-      ...config.webpackHardCache && hardSourcePlugin(cacheDir, config),
     ],
 
     entry: {
