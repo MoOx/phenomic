0dbe4ffd91c1162fc6f3f29327229aca88f1a4c1
Khoa Nguyen <hi@khoanguyen.me>
2016-09-04 03:27:13 +0700

    Remove try...catch block

--- src/bin/check-engine.js
+++ src/bin/check-engine.js
@@ -5,56 +5,36 @@ import colors from "chalk"
 
 module.exports = function() {
   const requirements = pkg.engines
-  const nodeVersion = process.env.FAKE_NODE_VERSION || process.version
+  const nodeVersion = process.env.TESTING
+    ? process.env.FAKE_NODE_VERSION
+    : process.version
+
   const npm = /^win/.test(process.platform) ? "npm.cmd" : "npm"
+  const npmVersion = process.env.TESTING ?
+    process.env.FAKE_NPM_VERSION :
+    execSync(npm + " --version").toString().trim()
 
-  try {
-    const stdout = execSync(npm + " --version")
-    const npmVersion = process.env.FAKE_NPM_VERSION || stdout.toString().trim()
-    if (!(
-      semver.satisfies(nodeVersion, requirements.node) &&
-      semver.satisfies(npmVersion, requirements.npm)
-    )) {
-      const errorMessage = colors.yellow(
-        "\n⚠️ " + "Phenomic requires at least " +
-        "node@" + requirements.node + " and npm@" + requirements.npm +
-        "\n\n" +
-        "Your node version is " + nodeVersion +
-        " and your npm version is " + npmVersion +
-        "\n\n" +
-        colors.yellow("See 'Setup' instruction in documentation.") +
-        " " +
-        "https://phenomic.io/docs/setup/"
-      )
-
-      if (process.env.TESTING) {
-        throw new Error(errorMessage)
-      }
-
-      console.log(errorMessage)
-      process.exit(0)
-    }
-  }
-  catch (err) {
-    if (process.env.TESTING) {
-      throw err
-    }
-    // I really don't know when will this happen
-    // If a user doesn't have npm, how can they even run this
+  if (!(
+    semver.satisfies(nodeVersion, requirements.node) &&
+    semver.satisfies(npmVersion, requirements.npm)
+  )) {
     const errorMessage = colors.yellow(
-      "\n⚠️ " + "Phenomic can't check your npm version." +
-      "\n\n"+
-      "Make sure you installed " +
+      "\n⚠️ " + "Phenomic requires at least " +
       "node@" + requirements.node + " and npm@" + requirements.npm +
-      "correctly" +
+      "\n\n" +
+      "Your node version is " + nodeVersion +
+      " and your npm version is " + npmVersion +
       "\n\n" +
       colors.yellow("See 'Setup' instruction in documentation.") +
       " " +
       "https://phenomic.io/docs/setup/"
     )
+
+    if (process.env.TESTING) {
+      throw new Error(errorMessage)
+    }
+
     console.log(errorMessage)
-    console.log(colors.red("\n Error stack trace: "))
-    console.error(err)
     process.exit(0)
   }
 }
