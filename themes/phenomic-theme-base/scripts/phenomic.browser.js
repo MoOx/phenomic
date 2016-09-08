784f1bd85bd65bf7465180de787c6af453157e83
Maxime Thirouin <m@moox.io>
2016-09-09 01:00:09 +0200

    Simplify scritpts/phenomic.*.js

--- themes/phenomic-theme-base/scripts/phenomic.browser.js
+++ themes/phenomic-theme-base/scripts/phenomic.browser.js
@@ -7,36 +7,26 @@ import "whatwg-fetch"
 import metadata from "../src/metadata.js"
 import routes from "../src/routes.js"
 import store from "../src/store.js"
-
 import phenomicClient from "phenomic/lib/client"
+phenomicClient({ metadata, routes, store })
 
-phenomicClient({
-  metadata,
-  routes,
-  store,
-})
+// md files processed via phenomic-loader to JSON && generate collection
+const mdContext = require.context("../content", true, /\.md$/)
+mdContext.keys().forEach(mdContext)
 
 // hot loading
-// md files → JSON && generate collection + hot loading for dev
-let mdContext = require.context("../content", true, /\.md$/)
-mdContext.keys().forEach(mdContext)
 if (module.hot) {
+
+  // hot load md
   const mdHotUpdater = require("phenomic/lib/client/hot-md").default
   module.hot.accept(mdContext.id, () => {
-    mdContext = require.context("../content", true, /\.md$/)
+    // mdContext = require.context("../content", true, /\.md$/)
     const requireUpdate = mdHotUpdater(mdContext, window.__COLLECTION__, store)
     mdContext.keys().forEach(requireUpdate)
   })
 
-  module.hot.accept([
-    "../src/metadata.js",
-    "../src/routes.js",
-    "../src/store.js",
-  ], () => {
-    phenomicClient({
-      metadata,
-      routes,
-      store,
-    })
-  })
+  module.hot.accept(
+    [ "../src/metadata.js", "../src/routes.js", "../src/store.js" ],
+    () => phenomicClient({ metadata, routes, store })
+  )
 }
--- themes/phenomic-theme-base/scripts/phenomic.node.js
+++ themes/phenomic-theme-base/scripts/phenomic.node.js
@@ -1,13 +1,7 @@
 import metadata from "../src/metadata.js"
 import routes from "../src/routes.js"
 import store from "../src/store.js"
-
 import phenomicStatic from "phenomic/lib/static"
 
 module.exports = (options) =>
-  phenomicStatic({
-    ...options,
-    metadata,
-    routes,
-    store,
-  })
+  phenomicStatic({ ...options, metadata, routes, store })
