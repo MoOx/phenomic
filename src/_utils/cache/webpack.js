3304884851d9572a697af4979a478d1b7d906b0a
Maxime Thirouin <m@moox.io>
2016-09-27 07:32:35 +0200

    Enable cache for webpack 2 as an update fix the issue
    Closes #777

--- src/_utils/cache/webpack.js
+++ src/_utils/cache/webpack.js
@@ -4,25 +4,9 @@ import { join } from "path"
 import HardSourceWebpackPlugin from "hard-source-webpack-plugin"
 import ObjectHash from "node-object-hash"
 import findCacheDir from "find-cache-dir"
-import webpackVersion from "../webpack-version"
-import log from "../log"
-
-let warned = false
 
 export default (config: PhenomicConfig): Array<any> => {
   if (!config.cache) {
-    return []
-  }
-
-  if (webpackVersion() === 2) {
-    if (!warned) {
-      log(
-        "Phenomic 'cache' option is not compatible yet with webpack@2 so the " +
-        "option is ignored. \n" +
-        "Please track https://github.com/MoOx/phenomic/issues/777"
-      )
-      warned = true
-    }
     return []
   }
 
