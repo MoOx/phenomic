d1f2a668aa36e3fe6753519e81e90e964bdd85e7
Ernie Turner <ernie.turner@gmail.com>
2016-09-30 00:27:12 -0600

    Fix commented out example for global CSS to be valid JS syntax (#797)
    * Fix commented out example for global CSS to be valid JS syntax
    
    * Fix commented out example for global CSS to be valid JS syntax (adjustements)

--- themes/phenomic-theme-base/webpack.config.babel.js
+++ themes/phenomic-theme-base/webpack.config.babel.js
@@ -159,15 +159,16 @@ export default (config = {}) => {
         //   // for global CSS if you want to keep CSS Modules by default
         //   // for your own CSS. If so, uncomment the line below
         //   // include: path.resolve(__dirname, "node_modules"),
-        //   loader: ExtractTextPlugin.extract(
+        //   loader: ExtractTextPlugin.extract({
         //     fallbackLoader: "style-loader",
         //     loader: [
-        //      "css-loader",
-        //      {
-        //        loader: "postcss-loader",
-        //        query: { plugins: postcssPlugins },
-        //      },
-        //   ),
+        //       "css-loader",
+        //       {
+        //         loader: "postcss-loader",
+        //         query: { "plugins": postcssPlugins },
+        //       },
+        //     ]
+        //   }),
         // },
         // ! \\ if you want to use Sass or LESS, you can add sass-loader or
         // less-loader after postcss-loader (or replacing it).
