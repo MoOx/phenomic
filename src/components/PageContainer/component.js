4d54be239a87a5a4761ce8f178a89c3d48e1d2ce
davidwells <david@inboundnow.com>
2016-10-23 18:02:44 -0700

    pass in additional page data to use in the custom loading component

--- src/components/PageContainer/component.js
+++ src/components/PageContainer/component.js
@@ -206,13 +206,15 @@ class PageContainer extends Component<DefaultProps, Props, void> {
     // page url from redux store
     const page = props.pages[pageUrl]
     let pageType = (page) ? page.type : ""
+    let loadingData
     // SSR window check
     if (typeof window !== "undefined" && window.__COLLECTION__) {
       // use window collection instead of page props
       const pageFromCollection = window.__COLLECTION__.filter((pageData) => {
         return pageUrl === pageData.__url
       })
-      pageType = pageFromCollection[0].layout
+      loadingData = pageFromCollection[0]
+      pageType = loadingData.layout
     }
     if (!page) {
       if (isDevelopmentClient()) {
@@ -256,7 +258,7 @@ class PageContainer extends Component<DefaultProps, Props, void> {
           Show custom loading during data fetch [LINK TO DOCS]`
         )
         // use normal layout and set isLoading prop to true
-        return <Layout isLoading />
+        return <Layout loadingData={ loadingData } isLoading />
       }
       else if (page.loading && PageLoading) {
         props.logger.info(
