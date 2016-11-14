4e6c05b7a1dea6a7f45cb4b40ea3a73d95bb218f
Maxime Thirouin <m@moox.io>
2016-11-14 22:14:45 +0100

    Added: hot loading for PostCSS config in base theme
    [A discussion is now open to push this solution in
    ``postcss-loader``](https://github.com/postcss/postcss-loader/issues/66#
    issuecomment-260214570)
      but until it is, you can use this solution to get hot loading for
    your PostCSS
      config. Very handy for development.

---
+++ themes/phenomic-theme-base/postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = (config) => [
+    require("stylelint")(),
+    require("postcss-cssnext")({
+      browsers: "last 2 versions",
+      features: {
+        customProperties: {
+          variables: {
+            mainColor: "#111",
+            mainColorContrasted: "#eee",
+          },
+        },
+      },
+    }),
+    require("postcss-reporter")(),
+    ...!config.production ? [
+      require("postcss-browser-reporter")(),
+    ] : [],
+  ]
