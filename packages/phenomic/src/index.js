7ffd6936bd40a5a47818f51864a6eb86ed286e56
Maxime Thirouin <m@moox.io>
2017-01-24 12:59:49 +0100

    Add poor validation for plugins

--- packages/phenomic/src/index.js
+++ packages/phenomic/src/index.js
@@ -7,13 +7,33 @@ import flattenConfiguration from "./configuration/flattenConfiguration"
 import start from "./commands/start"
 import build from "./commands/build"
 
+const normalizePlugin = (plugin) => {
+  if (!plugin) {
+    throw new Error(
+      "phenomic: You provided an undefined plugin"
+    )
+  }
+
+  if (typeof plugin !== "function") {
+    throw new Error(
+      "phenomic: You provided an plugin with type is " +
+      typeof plugin +
+      ". But function is expected instead of " +
+      plugin
+    )
+  }
+
+  // @todo send config here
+  return plugin()
+}
+
 function normalizeConfiguration(config: PhenomicInputConfig): PhenomicConfig {
   return {
     path: config.path || process.cwd(),
     outdir: config.outdir || path.join(process.cwd(), "dist"),
     bundler: config.bundler(),
     renderer: config.renderer(),
-    plugins: flattenConfiguration(config).map(plugin => plugin()),
+    plugins: flattenConfiguration(config).map(normalizePlugin),
     port: config.port || 1414,
   }
 }
