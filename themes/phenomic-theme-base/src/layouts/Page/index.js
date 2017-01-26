74b482d90613f7584861dffbecda20fe4481dcd2
Maxime Thirouin <m@moox.io>
2017-01-26 08:18:39 +0100

    Fix head.hero undefined

--- themes/phenomic-theme-base/src/layouts/Page/index.js
+++ themes/phenomic-theme-base/src/layouts/Page/index.js
@@ -30,7 +30,7 @@ const Page = (
 
   const metaTitle = head.metaTitle ? head.metaTitle : head.title
 
-  const socialImage = head.hero.match("://") ? head.hero
+  const socialImage = head.hero && head.hero.match("://") ? head.hero
     : joinUri(process.env.PHENOMIC_USER_URL, head.hero)
 
   const meta = [
