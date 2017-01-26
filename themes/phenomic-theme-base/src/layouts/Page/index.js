cfc489eef9938e2214f3552336770515f2bf5a0d
Vagish Vela <web@vagish.com>
2017-01-26 02:42:11 -0500

    Base theme: Add fb/twitter card image support (#934)
    * Add twitter card image support for local image paths
    
    * Fix error for over 80 characters on the line
    
    * Check if external url for twitter/og image
    
    * Account for protocol-less urls in RegExp test
    
    * Simplify the regexr check for image url
    
    * Fix head.hero undefined

--- themes/phenomic-theme-base/src/layouts/Page/index.js
+++ themes/phenomic-theme-base/src/layouts/Page/index.js
@@ -30,6 +30,9 @@ const Page = (
 
   const metaTitle = head.metaTitle ? head.metaTitle : head.title
 
+  const socialImage = head.hero && head.hero.match("://") ? head.hero
+    : joinUri(process.env.PHENOMIC_USER_URL, head.hero)
+
   const meta = [
     { property: "og:type", content: "article" },
     { property: "og:title", content: metaTitle },
@@ -37,11 +40,13 @@ const Page = (
       property: "og:url",
       content: joinUri(process.env.PHENOMIC_USER_URL, __url),
     },
+    { property: "og:image", content: socialImage },
     { property: "og:description", content: head.description },
     { name: "twitter:card", content: "summary" },
     { name: "twitter:title", content: metaTitle },
     { name: "twitter:creator", content: `@${ pkg.twitter }` },
     { name: "twitter:description", content: head.description },
+    { name: "twitter:image", content: socialImage },
     { name: "description", content: head.description },
   ]
 
