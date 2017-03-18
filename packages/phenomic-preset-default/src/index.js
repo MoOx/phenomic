6fa137f8560059fb8d7df69f97dfb396e3e05d34
Maxime Thirouin <m@moox.io>
2017-03-18 10:23:23 +0100

    Prepare phenomic-preset-default

--- packages/phenomic-preset-default/src/index.js
+++ packages/phenomic-preset-default/src/index.js
@@ -1,15 +1,17 @@
+import bundlerWebpack from "phenomic-plugin-bundler-webpack"
+import rendererReact from "phenomic-plugin-renderer-react"
 import transformMarkdown from "phenomic-plugin-transform-markdown"
+import directoryCollector from "phenomic-plugin-directory-collector"
 import transformJson from "phenomic-plugin-transform-json"
 import apiRelatedContent from "phenomic-plugin-api-related-content"
 
-export default {
-  plugins: {
-    transform: [
-      transformMarkdown,
-      transformJson,
-    ],
-    api: [
-      apiRelatedContent,
-    ],
-  },
+export default function() {
+  return [
+    bundlerWebpack,
+    rendererReact,
+    transformMarkdown,
+    transformJson,
+    directoryCollector,
+    apiRelatedContent,
+  ]
 }
