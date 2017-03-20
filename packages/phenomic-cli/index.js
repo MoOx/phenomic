6983710f3c25703368d1d90730d347c7e390326e
Maxime Thirouin <m@moox.io>
2017-03-20 07:00:45 +0100

    fix(phenomic-cli): use babel-register instead of babel-node for the bin

--- packages/phenomic-cli/index.js
+++ packages/phenomic-cli/index.js
@@ -1,3 +1,7 @@
-#!/usr/bin/env babel-node
+#!/usr/bin/env node
+
+// this babel register is mainly so you can have a webpack config file in es6
+// without a .babel extension
+require("babel-register")
 
 require("./lib/bin.js")
