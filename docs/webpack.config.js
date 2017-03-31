9f9f687b5638e9550b51eb99f9a4cc0c31de7031
Maxime Thirouin <m@moox.io>
2017-03-31 09:16:17 +0200

    Docs: remove webpack 1 support

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -5,10 +5,6 @@ import ExtractTextPlugin from "extract-text-webpack-plugin"
 import { phenomicLoader } from "phenomic"
 import PhenomicLoaderFeedWebpackPlugin
   from "phenomic/lib/loader-feed-webpack-plugin"
-/**
- * WARNING: this config is super ultra cool as it support both webpack 1 & 2
- */
-import webpackVersion from "phenomic/lib/_utils/webpack-version"
 
 import pkg from "./package.json"
 
@@ -28,24 +24,18 @@ export default (config = {}) => {
     },
     module: {
       noParse: /\.min\.js/,
-      [webpackVersion() === 1 ? "loaders" : "rules"]: [
+      "rules": [
         {
           // phenomic requirement
           test: /\.(md|markdown)$/,
           loader: phenomicLoader,
-          ...(
-            webpackVersion() === 1
-            ? { /* see below in global section */ }
-            : {
-              query: {
-                context: path.join(__dirname, config.source),
-                // plugins: [
-                //   ...require("phenomic/lib/loader-preset-markdown").default
-                // ]
-                // see https://phenomic.io/docs/usage/plugins/
-              },
-            }
-          ),
+          query: {
+            context: path.join(__dirname, config.source),
+            // plugins: [
+            //   ...require("phenomic/lib/loader-preset-markdown").default
+            // ]
+            // see https://phenomic.io/docs/usage/plugins/
+          },
         },
         {
           test: /\.json$/,
@@ -69,74 +59,46 @@ export default (config = {}) => {
           test: /\.css$/,
           exclude: /\.global\.css$/,
           include: path.resolve(__dirname, "src"),
-          loader: (
-            webpackVersion() === 1
-            ? (
-              ExtractTextPlugin.extract(
-                "style-loader",
-                [ `css-loader?modules&localIdentName=${
-                  config.production
-                  ? "[hash:base64:5]"
-                  : "[path][name]--[local]--[hash:base64:5]"
-                  }`,
-                  "postcss-loader",
-                ].join("!"),
-              )
-            )
-            : (
-              ExtractTextPlugin.extract({
-                fallbackLoader: "style-loader",
-                loader: [
-                  {
-                    loader: "css-loader",
-                    query: {
-                      modules: true,
-                      localIdentName: (
-                          config.production
-                          ? "[hash:base64:5]"
-                          : "[path][name]--[local]--[hash:base64:5]"
-                        ),
-                    },
-                  },
-                  {
-                    loader: "postcss-loader",
-                    // query for postcss can't be used right now
-                    // https://github.com/postcss/postcss-loader/issues/99
-                    // meanwhile, see webpack.LoaderOptionsPlugin in "plugins"
-                    // query: { plugins: postcssPlugins },
-                  },
-                ],
-              })
-            )
-          ),
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: "style-loader",
+            loader: [
+              {
+                loader: "css-loader",
+                query: {
+                  modules: true,
+                  localIdentName: (
+                      config.production
+                      ? "[hash:base64:5]"
+                      : "[path][name]--[local]--[hash:base64:5]"
+                    ),
+                },
+              },
+              {
+                loader: "postcss-loader",
+                // query for postcss can't be used right now
+                // https://github.com/postcss/postcss-loader/issues/99
+                // meanwhile, see webpack.LoaderOptionsPlugin in "plugins"
+                // query: { plugins: postcssPlugins },
+              },
+            ],
+          }),
         },
         {
           test: /\.global\.css$/,
           include: path.resolve(__dirname, "src"),
-          loader: (
-            webpackVersion() === 1
-            ? (
-              ExtractTextPlugin.extract(
-                "style-loader",
-                [ "css-loader", "postcss-loader" ].join("!"),
-              )
-            )
-            : (
-              ExtractTextPlugin.extract({
-                fallbackLoader: "style-loader",
-                loader: [
-                  "css-loader",
-                  {
-                    loader: "postcss-loader",
-                    // query for postcss can't be used right now
-                    // https://github.com/postcss/postcss-loader/issues/99
-                    // meanwhile, see webpack.LoaderOptionsPlugin in "plugins"
-                    // query: { plugins: postcssPlugins },
-                  },
-                ],
-              })
-            )
-          ),
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: "style-loader",
+            loader: [
+              "css-loader",
+              {
+                loader: "postcss-loader",
+                // query for postcss can't be used right now
+                // https://github.com/postcss/postcss-loader/issues/99
+                // meanwhile, see webpack.LoaderOptionsPlugin in "plugins"
+                // query: { plugins: postcssPlugins },
+              },
+            ],
+          }),
         },
         {
           test: /\.(html|ico|jpe?g|png|gif)$/,
@@ -153,43 +115,22 @@ export default (config = {}) => {
       ],
     },
 
-    ...(
-      webpackVersion() === 1
-      ? {
-        postcss: postcssPlugins,
-        phenomic: {
-          context: path.join(__dirname, config.source),
-          // plugins: [
-          //   ...require("phenomic/lib/loader-preset-markdown").default
-          // ]
-          // see https://phenomic.io/docs/usage/plugins/
-        },
-      }
-      : {}
-    ),
-
     plugins: [
-      ...(
-        webpackVersion() === 1
-        ? []
-        : [
-          // You should be able to remove the block below when the following
-          // issue has been correctly handled (and postcss-loader supports
-          // "plugins" option directly in query, see postcss-loader usage above)
-          // https://github.com/postcss/postcss-loader/issues/99
-          new webpack.LoaderOptionsPlugin({
-            test: /\.css$/,
-            options: {
-              postcss: postcssPlugins,
-              // required to avoid issue css-loader?modules
-              // this is normally the default value, but when we use
-              // LoaderOptionsPlugin, we must specify it again, otherwise,
-              // context is missing (and css modules names can be broken)!
-              context: __dirname,
-            },
-          }),
-        ]
-      ),
+      // You should be able to remove the block below when the following
+      // issue has been correctly handled (and postcss-loader supports
+      // "plugins" option directly in query, see postcss-loader usage above)
+      // https://github.com/postcss/postcss-loader/issues/99
+      new webpack.LoaderOptionsPlugin({
+        test: /\.css$/,
+        options: {
+          postcss: postcssPlugins,
+          // required to avoid issue css-loader?modules
+          // this is normally the default value, but when we use
+          // LoaderOptionsPlugin, we must specify it again, otherwise,
+          // context is missing (and css modules names can be broken)!
+          context: __dirname,
+        },
+      }),
 
       new PhenomicLoaderFeedWebpackPlugin({
         // here you define generic metadata for your feed
@@ -211,28 +152,12 @@ export default (config = {}) => {
         },
       }),
 
-      ...(
-        webpackVersion() === 1
-        ? [
-          new ExtractTextPlugin("[name].[hash].css", { disable: config.dev }),
-        ]
-        : [
-          new ExtractTextPlugin({
-            filename: "[name].[hash].css",
-            disable: config.dev,
-          }),
-        ]
-      ),
+      new ExtractTextPlugin({
+        filename: "[name].[hash].css",
+        disable: config.dev,
+      }),
 
       ...config.production && [
-        // DedupePlugin does not work correctly with Webpack 2, yet ;)
-        // https://github.com/webpack/webpack/issues/2644
-        ...(
-          webpackVersion() === 1
-          ? [ new webpack.optimize.DedupePlugin() ]
-          : []
-        ),
-
         new webpack.optimize.UglifyJsPlugin(
           { compress: { warnings: false } }
         ),
@@ -245,18 +170,6 @@ export default (config = {}) => {
       filename: "[name].[hash].js",
     },
 
-    ...(
-      webpackVersion() === 1
-      ? {
-        resolve: {
-          extensions: [ ".js", ".json", "" ],
-          root: [ path.join(__dirname, "node_modules") ],
-        },
-        resolveLoader: { root: [ path.join(__dirname, "node_modules") ] },
-      }
-      : {
-        resolve: { extensions: [ ".js", ".json" ] },
-      }
-    ),
+    resolve: { extensions: [ ".js", ".json" ] },
   }
 }
