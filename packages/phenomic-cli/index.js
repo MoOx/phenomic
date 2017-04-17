d7bf7993fb69a52f96d4b10830791c4e73c66853
Maxime Thirouin <m@moox.io>
2017-04-17 07:42:25 +0200

    feat(phenomic-cli): enable sourcemap

--- packages/phenomic-cli/index.js
+++ packages/phenomic-cli/index.js
@@ -2,7 +2,9 @@
 
 // this babel register is mainly so you can have a webpack config file in es6
 // without a .babel extension
-require("babel-register")
+require("babel-register")({
+  sourceMap: process.env.DEBUG !== "" ? "inline": undefined,
+})
 
 // $FlowFixMe lib/* are ignored
 require("./lib/bin.js")
