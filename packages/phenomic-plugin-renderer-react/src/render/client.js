5c07fb2acc2375d01e76a8625564c0f9917af702
Maxime Thirouin <m@moox.io>
2017-04-20 22:35:07 +0200

    fix(phenomic-plugin-renderer-react): client side rendering need routes
    Flow cannot validate children… :’(

--- packages/phenomic-plugin-renderer-react/src/render/client.js
+++ packages/phenomic-plugin-renderer-react/src/render/client.js
@@ -25,7 +25,7 @@ function render(routes: () => React$Element<any>) {
 
   ReactDOM.render(
     <Provider fetch={createFetchFunction()} store={store}>
-      {routes}
+      {routes()}
     </Provider>,
     document.getElementById("PhenomicRoot"),
   )
