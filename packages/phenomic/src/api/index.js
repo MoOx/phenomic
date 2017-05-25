e60908771e3538c7bb895ca57a10ebcb069a89bd
Matthias Le Brun <mlbli@me.com>
2017-05-25 13:17:47 +0200

    Add bidirection navigation for pagination

--- packages/phenomic/src/api/index.js
+++ packages/phenomic/src/api/index.js
@@ -7,11 +7,17 @@ const debug = require("debug")("phenomic:core:api");
 const encode = text => new Buffer(text).toString("base64");
 const decode = text => new Buffer(text, "base64").toString();
 
-const connect = (list, limit) => {
+const connect = (list, limit, previousList = []) => {
   const hasNextPage = limit === undefined ? false : list.length >= limit;
+  const hasPreviousPage = previousList.length > 0;
   return {
+    hasPreviousPage,
+    previousPageIsFirst: previousList.length <= limit,
+    previous: hasPreviousPage
+      ? encode(previousList[previousList.length - 2].key)
+      : null,
     hasNextPage,
-    next: hasNextPage ? encode(list[list.length - 2].key) : null,
+    next: hasNextPage ? encode(list[list.length - 1].key) : null,
     list: list.slice(0, limit)
   };
 };
@@ -78,23 +84,35 @@ function createServer(db: PhenomicDB, plugins: PhenomicPlugins) {
       debug(req.url, JSON.stringify(req.params));
       try {
         const limit = parseInt(req.params.limit);
-        const lt = decode(req.params.after);
+        const lte = decode(req.params.after);
         // @todo check lt validity (exist?); otherwise, trigger an error (404?)
         // cause during dev all "lt" are responding 200, even random values
         // but in production, it's not the case as only known values are
         // generated as endpoints
         const reverse = req.params.order === "desc";
-        const list = await db.getList(
-          req.params.collection,
-          {
-            limit: limit + 1,
-            lt,
-            reverse
-          },
-          req.params.filter,
-          req.params.value
-        );
-        res.json(connect(list, limit));
+        const [list, previousList] = await Promise.all([
+          db.getList(
+            req.params.collection,
+            {
+              limit: limit + 1,
+              lte,
+              reverse
+            },
+            req.params.filter,
+            req.params.value
+          ),
+          db.getList(
+            req.params.collection,
+            {
+              limit: limit + 1,
+              gt: lte,
+              reverse: !reverse
+            },
+            req.params.filter,
+            req.params.value
+          )
+        ]);
+        res.json(connect(list, limit, previousList));
       } catch (error) {
         console.error(error);
         res.status(404).end();
