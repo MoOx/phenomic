e26a14e828f7badaef9d4a4e5f31d7aa2adcbd21
Maxime Thirouin <m@moox.io>
2017-06-01 15:57:14 +0200

    docs: start of a new Home (just a start ;)

--- docs/components/Wrapper/index.js
+++ docs/components/Wrapper/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { View } from "react-primitives";
+import { StyleSheet, View } from "react-primitives";
 import Head from "react-helmet";
 
 const Wrapper = (props: Object) => (
-  <View>
+  <View style={styles.wrapper}>
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
-    <View>
-      {props.children}
-    </View>
+    {props.children}
   </View>
 );
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: "#eff7f7"
+  }
+});
 
 export default Wrapper;
