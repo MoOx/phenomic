b3496a4fb967d588aed132ad5cd21f9c70de7dea
Maxime Thirouin <m@moox.io>
2017-06-01 15:57:39 +0200

    docs: remove whatwg-fetch as it's included already

--- docs/App.js
+++ docs/App.js
@@ -1,5 +1,3 @@
-import "whatwg-fetch";
-
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
 import { createApp, renderApp } from "@phenomic/preset-react-app/lib/client";
