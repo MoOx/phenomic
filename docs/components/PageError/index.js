c43f98119bb6143acfc736b88b7340790a47911b
Maxime Thirouin <m@moox.io>
2017-06-01 15:58:39 +0200

    docs: better 404/error page

--- docs/components/PageError/index.js
+++ docs/components/PageError/index.js
@@ -1,17 +1,58 @@
 import React from "react";
-import { View } from "react-primitives";
-import Head from "react-helmet";
+import { StyleSheet, View, Text } from "react-primitives";
+
+import BodyContainer from "../BodyContainer";
+import Header from "../Header";
 
 const PageError = ({ error }: { error: Object }) => {
   const status = (error && error.status) || 404;
-  const message = error && status !== 404 ? error.statusText : "Page not found";
+  const errorText = error && status !== 404
+    ? error.statusText
+    : "Page not found";
 
   return (
     <View>
-      <Head><title>{message}</title></Head>
-      <h1>{message}</h1>
+      <Header title={errorText} />
+      <BodyContainer style={styles.container}>
+        <Text style={styles.oops}>{"Oooops!"}</Text>
+        <View style={styles.text}>
+          <Text style={styles.title}>
+            <strong>{status}</strong>
+            {" "}
+            {errorText}
+          </Text>
+          {status === 404 &&
+            <View>
+              {"It seems you found a broken link. "}
+              {"Sorry about that. "}
+              <br />
+              {"Do not hesitate to report this page."}
+            </View>}
+        </View>
+      </BodyContainer>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "50vh",
+    padding: "1rem 0"
+  },
+  oops: {
+    fontSize: "4rem",
+    lineHeight: "4rem",
+    color: "#ddd"
+  },
+  title: {
+    margin: 60,
+    fontSize: 24,
+    lineHeight: 48,
+    textAlign: "center"
+  }
+});
 export default PageError;
