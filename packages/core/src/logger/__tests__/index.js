31fa53eab7483cf979d54b060bad56cb0cc15e37
Maxime Thirouin <m@moox.io>
2017-06-09 05:20:20 +0200

    chore: try skipping test on snapshots that produce platform specific result (logger icons)

--- packages/core/src/logger/__tests__/index.js
+++ packages/core/src/logger/__tests__/index.js
@@ -2,10 +2,15 @@ import logger from "..";
 
 test("log should log", () => {
   const log = logger("sender test", false);
-  expect(log("test msg")).toMatchSnapshot();
-  expect(log.debug("test debug")).toMatchSnapshot();
-  expect(log.info("test info")).toMatchSnapshot();
-  expect(log.success("test success")).toMatchSnapshot();
-  expect(log.warn("test warning")).toMatchSnapshot();
-  expect(log.error("test error")).toMatchSnapshot();
+  if (process.platform !== "win32") {
+    expect(log("test msg")).toMatchSnapshot();
+    expect(log.debug("test debug")).toMatchSnapshot();
+    expect(log.info("test info")).toMatchSnapshot();
+    expect(log.success("test success")).toMatchSnapshot();
+    expect(log.warn("test warning")).toMatchSnapshot();
+    expect(log.error("test error")).toMatchSnapshot();
+  } else {
+    expect(log("test msg").includes("test msg")).toBe(true);
+    expect(log("test msg").includes("sender test")).toBe(true);
+  }
 });
