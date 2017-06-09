6bd42744ca4218b1dd656c834cca1ee9fb450f56
Maxime Thirouin <m@moox.io>
2017-06-09 06:53:19 +0200

    chore: no snapshots for packages/core/src/logger/__tests__/index.js

--- packages/core/src/logger/__tests__/index.js
+++ packages/core/src/logger/__tests__/index.js
@@ -1,16 +1,24 @@
+import logSymbols from "log-symbols";
+
 import logger from "..";
 
 test("log should log", () => {
   const log = logger("sender test", false);
-  if (process.platform !== "win32") {
-    expect(log("test msg")).toMatchSnapshot();
-    expect(log.debug("test debug")).toMatchSnapshot();
-    expect(log.info("test info")).toMatchSnapshot();
-    expect(log.success("test success")).toMatchSnapshot();
-    expect(log.warn("test warning")).toMatchSnapshot();
-    expect(log.error("test error")).toMatchSnapshot();
-  } else {
-    expect(log("test msg").includes("test msg")).toBe(true);
-    expect(log("test msg").includes("sender test")).toBe(true);
-  }
+  // we can't snapshot because of logSymbols
+  expect(log("test msg")).toBe(
+    `${logSymbols.info} [90msender test: [39m test msg`
+  );
+  expect(log.debug("test debug")).toBe(`  [90msender test: [39m test debug`);
+  expect(log.info("test info")).toBe(
+    `${logSymbols.info} [90msender test: [39m test info`
+  );
+  expect(log.success("test success")).toBe(
+    `${logSymbols.success} [90msender test: [39m test success`
+  );
+  expect(log.warn("test warning")).toBe(
+    `${logSymbols.warning} [90msender test: [39m test warning`
+  );
+  expect(log.error("test error")).toBe(
+    `${logSymbols.error} [90msender test: [39m test error`
+  );
 });
