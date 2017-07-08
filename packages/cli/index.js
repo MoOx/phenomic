3b88cc8d143589aba91b9d657586dc95fd923879
Maxime Thirouin <m@moox.io>
2017-07-08 23:04:09 +0200

    feat(@phenomic/cli): add a default babel config
    Closes #1071

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
+// require by babel-preset-react-app
+// This is used like this since babel transformed file don't finish
+// on the client
+// webpack (or similar) handle their own env.
+// anyway it's redifined a bit later by start/build commands
+process.env.BABEL_ENV = "development";
+
 // this babel register is mainly so you can have a webpack config file in es6
 // without a .babel extension
 require("babel-register")({
-  sourceMap: process.env.DEBUG !== "" ? "inline" : undefined
+  presets: [require.resolve("@phenomic/babel-preset")],
+  sourceMap: "inline"
 });
 
 // $FlowFixMe lib/* are ignored
