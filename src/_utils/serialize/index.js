50b3baa5299eab4dbc960b5db16d7ea1fa8d759f
Julien Bouquillon <julien@revolunet.com>
2017-07-24 16:33:39 +0200

    fix unicode newlines issue for #1125

--- src/_utils/serialize/index.js
+++ src/_utils/serialize/index.js
@@ -1,5 +1,5 @@
 // @flow
 
 export default (value: any): string => {
-  return JSON.stringify(value).replace(/\<\/script>/g, "<\\/script>")
+  return JSON.stringify(value).replace(/\<\/script>/g, "<\\/script>").replace(/\u2028/g,"\\u2028").replace(/\u2029/g,"\\u2029")
 }
