e614dd3a511e5f1db17ce0a2fb2172257225ae90
Maxime Thirouin <157534+MoOx@users.noreply.github.com>
2017-08-19 22:13:13 +0200

    Update index.js

--- src/_utils/serialize/index.js
+++ src/_utils/serialize/index.js
@@ -1,5 +1,6 @@
 // @flow
 
 export default (value: any): string => {
-  return JSON.stringify(value).replace(/\<\/script>/g, "<\\/script>").replace(/\u2028/g,"\\u2028").replace(/\u2029/g,"\\u2029")
+  return JSON.stringify(value).replace(/\<\/script>/g, "<\\/script>")
+    .replace(/\u2028/g,"\\u2028").replace(/\u2029/g,"\\u2029")
 }
