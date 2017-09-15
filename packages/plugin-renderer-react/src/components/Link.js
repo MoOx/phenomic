5b7422465c7755e51ef07bac1a53a6bcaf4610ab
MoOx <m@moox.io>
2017-09-15 08:53:21 +0200

    Improve Link behavior for better accessibility (respect modifiers, target + keyboard navigation)
    The Link component (by default used to navigate between content (eg: markdown) link) has been improved:
    - does not use directly react-router Link, but instead just push the url the navigation history (which is supported by react-router
    - respect target=_blank original behavior
    - respect keyboard modifiers original behaviors (eg: cmd+click, alt+click etc)
    - work better with keyboard navigation (enter key) has it's now triggering client side navigation!
    
    This component can receive onClick and onPress callback that can trigger a preventDefault() to avoid the original behavior of the component if necessary.

--- packages/plugin-renderer-react/src/components/Link.js
+++ packages/plugin-renderer-react/src/components/Link.js
@@ -2,7 +2,7 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
-import { Link as RouterLink } from "react-router";
+import { browserHistory } from "react-router";
 
 const origin = url =>
   typeof url === "object" &&
@@ -12,6 +12,8 @@ const origin = url =>
     url.protocol + "//" + url.hostname + (url.port ? ":" + url.port : ""));
 
 type PropsType = {
+  style?: Object,
+  activeStyle?: Object,
   className?: string,
   activeClassName?: string,
   to?: string,
@@ -19,7 +21,45 @@ type PropsType = {
   children?: React.Node
 };
 
-function Link(props: PropsType, { router }: { router: Object }) {
+const shouldIgnoreEvent = (event: SyntheticEvent<*>) =>
+  // If target prop is set (e.g. to "_blank"), let browser handle link.
+  event.currentTarget.target ||
+  event.defaultPrevented ||
+  // modifier pressed
+  (event.metaKey || event.altKey || event.ctrlKey || event.shiftKey || false);
+
+const goToUrl = (event: SyntheticEvent<*>, url: string) => {
+  event.preventDefault();
+  browserHistory.push(url);
+};
+
+export const isActive = (url: string, { router }: Object) =>
+  router &&
+  router.isActive &&
+  (router.isActive({ pathname: url }) ||
+    router.isActive({ pathname: url + "index.html" }));
+
+export const handlePress = (url: string, props?: Object) => (
+  event: SyntheticMouseEvent<HTMLElement>
+) => {
+  props && props.onClick && props.onClick(event);
+  !shouldIgnoreEvent(event) &&
+    // left click
+    event.button === 0 &&
+    goToUrl(event, url);
+};
+
+export const handleKeyDown = (url: string, props?: Object) => (
+  event: SyntheticKeyboardEvent<HTMLElement>
+) => {
+  props && props.onKeyDown && props.onKeyDown(event);
+  !shouldIgnoreEvent(event) &&
+    // enter key
+    event.keyCode === 13 &&
+    goToUrl(event, url);
+};
+
+function Link(props: PropsType, context: Object) {
   // eslint-disable-next-line no-unused-vars
   const { to, href, ...otherProps } = props;
   const url = props.to || props.href || "";
@@ -34,6 +74,19 @@ function Link(props: PropsType, { router }: { router: Object }) {
   const toLink = document.createElement("a");
   toLink.href = url;
 
+  const isLocal =
+    // jsdom have url data directly into "url" property, so just in case
+    // it's like that on some browsers...
+    origin(toLink) === origin(window.location);
+  // we might want to restrict Link to path including the pathname
+  // but this will require to preprend pathname to all Links from the
+  // path, which sucks.
+  // If people wants to use Link for a same domain, but in the parent path,
+  // you will need to includes the entire url, / won't work at it will use
+  // the react-router basename defined by Phenomic.
+  // &&
+  // toLink.pathname.includes(process.env.PHENOMIC_USER_PATHNAME)
+  // toLink.pathname.indexOf(process.env.PHENOMIC_USER_PATHNAME) > -1
   // @todo handle properly use base only
   // in v0.x we were relying on the base url
   // if (
@@ -45,39 +98,29 @@ function Link(props: PropsType, { router }: { router: Object }) {
   //   return simpleLink;
   // }
 
-  if (
-    // jsdom have url data directly into "url" property, so just in case
-    // it's like that on some browsers...
-    origin(toLink) === origin(window.location)
-    // we might want to restrict Link to path including the pathname
-    // but this will require to preprend pathname to all Links from the
-    // path, which sucks.
-    // If people wants to use Link for a same domain, but in the parent path,
-    // you will need to includes the entire url, / won't work at it will use
-    // the react-router basename defined by Phenomic.
-    // &&
-    // toLink.pathname.includes(process.env.PHENOMIC_USER_PATHNAME)
-    // toLink.pathname.indexOf(process.env.PHENOMIC_USER_PATHNAME) > -1
-  ) {
-    const className = cx(props.className, {
-      [props.activeClassName || ""]:
-        router &&
-        (router.isActive({ pathname: url }) ||
-          router.isActive({ pathname: url + "index.html" })) &&
-        Boolean(props.activeClassName)
-    });
-    return (
-      <RouterLink
-        {...otherProps}
-        // react-router seems to be pretty dumb with relative links...
-        // this way we have a proper link
-        to={toLink.href}
-        {...(className ? { className } : {})}
-      />
-    );
+  if (!isLocal) {
+    return simpleLink;
   }
 
-  return simpleLink;
+  const isUrlActive = isActive(url, context);
+  const className = cx(props.className, isUrlActive && props.activeClassName);
+
+  return (
+    <a
+      {...otherProps}
+      href={toLink.href}
+      onClick={handlePress(toLink.href, props)}
+      onKeyDown={handleKeyDown(toLink.href, props)}
+      // @todo handle onKeyPress for keyboard navigation
+      // for now, it's falling back to normal links
+      style={{
+        ...props.style,
+        ...(isActive && props.activeStyle)
+      }}
+      // weird syntax to avoid undefined/empty classname
+      {...(className ? { className } : {})}
+    />
+  );
 }
 
 Link.contextTypes = {
