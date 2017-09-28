5c51946309708fda834b25d7fa834b6dc51d1342
MoOx <m@moox.io>
2017-09-28 07:58:10 +0200

    Windows compatibility

--- packages/core/src/logger/__tests__/index.js
+++ packages/core/src/logger/__tests__/index.js
@@ -4,19 +4,27 @@ import logger from "..";
 
 test("log should log", () => {
   const log = logger("sender test", false);
-  // we can't snapshot because of logSymbols
-  expect(log("test msg")).toBe(`${logSymbols.info} [90msender test: [39m test msg`);
-  expect(log.debug("test debug")).toBe(`  [90msender test: [39m test debug`);
+
+  // we can't snapshot because of logSymbols & colors...
+  const colorStart = process.platform !== "win32" ? "[90m" : "";
+  const colorEnd = process.platform !== "win32" ? "[39m" : "";
+
+  expect(log("test msg")).toBe(
+    `${logSymbols.info} ${colorStart}sender test: ${colorEnd} test msg`
+  );
+  expect(log.debug("test debug")).toBe(
+    `  ${colorStart}sender test: ${colorEnd} test debug`
+  );
   expect(log.info("test info")).toBe(
-    `${logSymbols.info} [90msender test: [39m test info`
+    `${logSymbols.info} ${colorStart}sender test: ${colorEnd} test info`
   );
   expect(log.success("test success")).toBe(
-    `${logSymbols.success} [90msender test: [39m test success`
+    `${logSymbols.success} ${colorStart}sender test: ${colorEnd} test success`
   );
   expect(log.warn("test warning")).toBe(
-    `${logSymbols.warning} [90msender test: [39m test warning`
+    `${logSymbols.warning} ${colorStart}sender test: ${colorEnd} test warning`
   );
   expect(log.error("test error")).toBe(
-    `${logSymbols.error} [90msender test: [39m test error`
+    `${logSymbols.error} ${colorStart}sender test: ${colorEnd} test error`
   );
 });
