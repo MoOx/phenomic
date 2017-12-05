d140a03a26a28b24472498c5c49f43aecc0b99fe
Cyrille Perois <cperois@outlook.com>
2017-12-05 17:43:35 +0100

    webpack config based on react-scripts

--- packages/plugin-bundler-webpack/src/webpack.config.js
+++ packages/plugin-bundler-webpack/src/webpack.config.js
@@ -3,22 +3,27 @@ import path from "path";
 import webpack from "webpack";
 import ExtractTextPlugin from "extract-text-webpack-plugin";
 
+const isStatic = process.env.PHENOMIC_ENV === "static";
+const isProduction = process.env.NODE_ENV === "production";
+
+const baseConfig = require("react-scripts/config/webpack.config." +
+  (isProduction ? "prod" : "dev"));
+
 module.exports = (config: PhenomicConfig) => ({
+  ...baseConfig,
   entry: {
     [config.bundleName]: [
-      process.env.PHENOMIC_ENV !== "static" &&
-        require.resolve("webpack-hot-middleware/client"),
-      process.env.PHENOMIC_ENV !== "static" &&
-        require.resolve("react-hot-loader/patch"),
+      !isStatic && require.resolve("webpack-hot-middleware/client"),
+      !isStatic && require.resolve("react-hot-loader/patch"),
       "./App.js"
     ].filter(item => item)
   },
   output: {
-    publicPath: "/", // @todo make this dynamic
+    ...baseConfig.output,
     path: path.isAbsolute(config.outdir)
       ? config.outdir
       : path.join(process.cwd(), config.outdir),
-    ...(process.env.PHENOMIC_ENV !== "static"
+    ...(!isStatic
       ? {
           filename: "phenomic/[name].js",
           chunkFilename: "phenomic/[name].chunk.js"
@@ -29,50 +34,38 @@ module.exports = (config: PhenomicConfig) => ({
         })
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: require.resolve("babel-loader"),
-        options: {
-          babelrc: false,
-          presets: [require.resolve("@phenomic/babel-preset")],
-          plugins: [require.resolve("react-hot-loader/babel")]
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: require.resolve("style-loader"),
-          use: require.resolve("css-loader")
-        })
+    ...baseConfig.module,
+    rules: baseConfig.module.rules.map(rule => {
+      if (rule.oneOf) {
+        return {
+          oneOf: rule.oneOf.map(rule => {
+            if (rule.loader === require.resolve("babel-loader")) {
+              return {
+                ...rule,
+                include: process.cwd(),
+                exclude: /node_modules/,
+                options: {
+                  ...rule.options,
+                  presets: ["@phenomic/babel-preset"],
+                  plugins: ["react-hot-loader/babel"]
+                }
+              };
+            }
+
+            return rule;
+          })
+        };
       }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: "phenomic/[name].[contenthash:8].css",
-      disable: process.env.PHENOMIC_ENV !== "static"
-    }),
-    process.env.PHENOMIC_ENV !== "static" &&
-      new webpack.HotModuleReplacementPlugin(),
-    process.env.NODE_ENV === "production" &&
-      new webpack.optimize.UglifyJsPlugin()
-  ].filter(item => item),
 
-  resolve: {
-    // react-native(-web) | react-primitives
-    extensions: [".web.js", ".js", ".json"],
-    alias: {
-      "react-native": "react-native-web"
+      return rule;
+    })
+  },
+  plugins: baseConfig.plugins.map(plugin => {
+    if (plugin instanceof ExtractTextPlugin) {
+      return new ExtractTextPlugin({
+        filename: "phenomic/[name].[contenthash:8].css",
+        disable: !isStatic
+      });
     }
-  },
-
-  // eslint-disable-next-line max-len
-  // https://github.com/facebookincubator/create-react-app/blob/fbdff9d722d6ce669a090138022c4d3536ae95bb/packages/react-scripts/config/webpack.config.prod.js#L279-L285
-  node: {
-    fs: "empty",
-    net: "empty",
-    tls: "empty"
-  }
+  })
 });
