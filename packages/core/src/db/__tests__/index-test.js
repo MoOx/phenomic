10469f4fa4a9d556cc8ae52ab4d7d024a57bcd47
Maxime Thirouin <m@moox.io>
2017-12-16 19:58:15 +0100

    🤫 `@phenomic/core`: db - ensure data returned with get/getList (internal)

--- packages/core/src/db/__tests__/index-test.js
+++ packages/core/src/db/__tests__/index-test.js
@@ -6,20 +6,36 @@ describe("db", () => {
     jest.resetModuleRegistry();
   });
 
-  let i = 0;
-
   it("should be able to put & get a value", () => {
     return db
-      .put("foo", "bar", { data: { title: "bar" } })
+      .put("foo", "bar", { data: { title: "bar" }, partial: { title: "bar" } })
       .then(() => db.get("foo", "bar"))
       .then(value => expect(value).toMatchSnapshot());
   });
 
+  it("should be able to get an item with relations", () => {
+    i++;
+    return Promise.all([
+      db.put("authors" + i, "foo", {
+        data: { name: "foo-name", bio: "foo-bio" },
+        partial: { name: "foo-name" }
+      }),
+      db.put("bar" + i, "foo", {
+        data: { ["authors" + i]: ["foo"] },
+        partial: { ["authors" + i]: ["foo"] }
+      })
+    ])
+      .then(() => db.get("bar" + i, "foo"))
+      .then(value => expect(value).toMatchSnapshot());
+  });
+
+  let i = 0;
+
   it("should be able to get a list", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "baz", { data: { title: "baz" } }),
-      db.put("bar" + i, "foo", { data: { title: "foo" } })
+      db.put("bar" + i, "baz", { data: {}, partial: { title: "title-baz" } }),
+      db.put("bar" + i, "foo", { data: {}, partial: { title: "title-foo" } })
     ])
       .then(() => db.getList("bar" + i, {}, "default", ""))
       .then(value => expect(value).toMatchSnapshot());
@@ -28,8 +44,8 @@ describe("db", () => {
   it("should be able to get a reversed list", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "baz", { data: { title: "baz" } }),
-      db.put("bar" + i, "foo", { data: { title: "foo" } })
+      db.put("bar" + i, "baz", { data: {}, partial: { title: "title-baz" } }),
+      db.put("bar" + i, "foo", { data: {}, partial: { title: "title-foo" } })
     ])
       .then(() => db.getList("bar" + i, { reverse: true }, "default", ""))
       .then(value => expect(value).toMatchSnapshot());
@@ -38,9 +54,9 @@ describe("db", () => {
   it("should be able to get a list after (starting) some value", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "baz", { data: { title: "baz" } }),
-      db.put("bar" + i, "eoo", { data: { title: "eoo" } }),
-      db.put("bar" + i, "foo", { data: { title: "foo" } })
+      db.put("bar" + i, "baz", { data: {}, partial: { title: "title-baz" } }),
+      db.put("bar" + i, "eoo", { data: {}, partial: { title: "title-eoo" } }),
+      db.put("bar" + i, "foo", { data: {}, partial: { title: "title-foo" } })
     ])
       .then(() => db.getList("bar" + i, { gte: "eoo" }, "default", ""))
       .then(value => expect(value).toMatchSnapshot());
@@ -49,9 +65,9 @@ describe("db", () => {
   it("should be able to get a list after some value", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "baz", { data: { title: "baz" } }),
-      db.put("bar" + i, "eoo", { data: { title: "eoo" } }),
-      db.put("bar" + i, "foo", { data: { title: "foo" } })
+      db.put("bar" + i, "baz", { data: {}, partial: { title: "title-baz" } }),
+      db.put("bar" + i, "eoo", { data: {}, partial: { title: "title-eoo" } }),
+      db.put("bar" + i, "foo", { data: {}, partial: { title: "title-foo" } })
     ])
       .then(() => db.getList("bar" + i, { gt: "eoo" }, "default", ""))
       .then(value => expect(value).toMatchSnapshot());
@@ -60,9 +76,9 @@ describe("db", () => {
   it("should be able to get a list before (starting) some value", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "baz", { data: { title: "baz" } }),
-      db.put("bar" + i, "eoo", { data: { title: "eoo" } }),
-      db.put("bar" + i, "foo", { data: { title: "foo" } })
+      db.put("bar" + i, "baz", { data: {}, partial: { title: "title-baz" } }),
+      db.put("bar" + i, "eoo", { data: {}, partial: { title: "title-eoo" } }),
+      db.put("bar" + i, "foo", { data: {}, partial: { title: "title-foo" } })
     ])
       .then(() => db.getList("bar" + i, { lte: "eoo" }, "default", ""))
       .then(value => expect(value).toMatchSnapshot());
@@ -71,9 +87,9 @@ describe("db", () => {
   it("should be able to get a list before some value", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "baz", { data: { title: "baz" } }),
-      db.put("bar" + i, "eoo", { data: { title: "eoo" } }),
-      db.put("bar" + i, "foo", { data: { title: "foo" } })
+      db.put("bar" + i, "baz", { data: {}, partial: { title: "title-baz" } }),
+      db.put("bar" + i, "eoo", { data: {}, partial: { title: "title-eoo" } }),
+      db.put("bar" + i, "foo", { data: {}, partial: { title: "title-foo" } })
     ])
       .then(() => db.getList("bar" + i, { lt: "eoo" }, "default", ""))
       .then(value => expect(value).toMatchSnapshot());
@@ -82,8 +98,8 @@ describe("db", () => {
   it("should be able to get a list with a limited count", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "baz", { data: { title: "baz" } }),
-      db.put("bar" + i, "foo", { data: { title: "foo" } })
+      db.put("bar" + i, "baz", { data: {}, partial: { title: "title-baz" } }),
+      db.put("bar" + i, "foo", { data: {}, partial: { title: "title-foo" } })
     ])
       .then(() => db.getList("bar" + i, { limit: 1 }, "default", ""))
       .then(value => expect(value).toMatchSnapshot());
@@ -96,9 +112,18 @@ describe("db", () => {
   it("should be able to get a list with a specific query", () => {
     i++;
     return Promise.all([
-      db.put("bar" + i, "foo", { data: { title: "foo", tags: ["a", "b"] } }),
-      db.put("bar" + i, "bar", { data: { title: "bar", tags: ["a", "c"] } }),
-      db.put("bar" + i, "baz", { data: { title: "baz", tags: ["b", "c"] } })
+      db.put("bar" + i, "foo", {
+        data: { title: "title-foo", tags: ["a", "b"] },
+        partial: { title: "title-foo" }
+      }),
+      db.put("bar" + i, "bar", {
+        data: { title: "title-bar", tags: ["a", "c"] },
+        partial: { title: "title-bar" }
+      }),
+      db.put("bar" + i, "baz", {
+        data: { title: "title-baz", tags: ["b", "c"] },
+        partial: { title: "title-baz" }
+      })
     ])
       .then(() => db.getList("bar" + i, {}, "tags", "a"))
       .then(value => expect(value).toMatchSnapshot());
