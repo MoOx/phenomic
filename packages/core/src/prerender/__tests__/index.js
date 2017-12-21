b19e8e994464aeef0810854748d070cf2b85eb73
Maxime Thirouin <m@moox.io>
2017-12-21 08:58:00 +0100

    🤫 `@phenomic/core`: cover prerender/resolve with more tests

--- packages/core/src/prerender/__tests__/index.js
+++ packages/core/src/prerender/__tests__/index.js
@@ -1,14 +1,15 @@
+import query from "@phenomic/api-client/lib/query";
+
 import resolve from "../resolve.js";
 
 const NoopComponent = {
   fakeComponent: true // to pass test on component being valid
 };
 
-const noopFetch = async (/*config: PhenomicQueryConfig*/) => {
-  return await Promise.resolve({
-    test: true
+const noopFetch = async (/*config: PhenomicQueryConfig*/) =>
+  await Promise.resolve({
+    list: []
   });
-};
 
 it("should be able to generate a single url", async () => {
   expect(
@@ -38,6 +39,35 @@ it("should be able to generate a multiple static url", async () => {
         }
       ],
       noopFetch
+    )
+  ).toMatchSnapshot();
+});
+
+it("should be able to resolve dynamic urls", async () => {
+  expect(
+    await resolve(
+      [
+        {
+          path: "/test/*",
+          component: {
+            getQueries: (/*params: PhenomicQueryConfig*/) => ({
+              test: query({
+                id: "one"
+                // path?: string,
+                // after?: string,
+                // by?: string,
+                // value?: string,
+                // order?: string,
+                // limit?: number
+              })
+            })
+          }
+        }
+      ],
+      async () =>
+        await Promise.resolve({
+          list: [{ id: "test-1" }, { id: "test-2" }]
+        })
     )
   ).toMatchSnapshot();
 });
