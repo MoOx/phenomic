00d31acd07201296b11244747af64ea36cdea882
Matthias Le Brun <mlbli@me.com>
2018-02-26 20:54:38 +0100

    @phenomic/api-client: Remove hasOwnProperty checks (#1233)

--- packages/api-client/src/query.js
+++ packages/api-client/src/query.js
@@ -4,7 +4,7 @@ function query(config: PhenomicQueryConfig): PhenomicQueryConfig {
   debug("query", config);
 
   // note that during static build, we initiate the query with no id
-  if (config.hasOwnProperty("id")) {
+  if (config.id !== undefined) {
     return {
       path: config.path,
       id: config.id
@@ -16,9 +16,7 @@ function query(config: PhenomicQueryConfig): PhenomicQueryConfig {
     value: config.by && config.value ? config.value : "1",
     order: config.order ? config.order : "desc",
     limit: config.limit ? parseInt(config.limit, 10) : undefined,
-    ...(config.hasOwnProperty("after") && config.after
-      ? { after: config.after }
-      : null)
+    ...(config.after !== undefined ? { after: config.after } : null)
   };
 }
 
