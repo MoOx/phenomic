6f21cd3764ba3dec7965e890315db5050b2be852
Maxime Thirouin <m@moox.io>
2018-03-08 06:56:42 +0100

    Website: add favicon & friends

--- website/components/Wrapper/index.js
+++ website/components/Wrapper/index.js
@@ -7,6 +7,27 @@ const Wrapper = (props: Object) => (
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     {props.children}
   </View>
