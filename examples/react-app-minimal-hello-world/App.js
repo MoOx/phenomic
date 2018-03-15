d78bb320a773d8d2f2a14d190df681c955d3d5d4
Maxime Thirouin <m@moox.io>
2018-03-15 08:15:01 +0100

    examples/react-app-minimal-hello-world

--- examples/react-app-minimal-hello-world/App.js
+++ examples/react-app-minimal-hello-world/App.js
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { Router, Route, browserHistory } from "react-router";
 import { createApp } from "@phenomic/preset-react-app/lib/client";
+// import { createApp, renderApp } from "@phenomic/preset-react-app/lib/client";
 
 export default createApp(() => (
   <Router history={browserHistory}>
     <Route path="/" component={() => <div>Hello world!</div>} />
   </Router>
 ));
+
+// uncommment to get hot loading
+// if (module.hot) {
+//   module.hot.accept(() => renderApp(routes));
+// }
