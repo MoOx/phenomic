39da98986ce5d3e67b70cabc201a98bc9b18408f
Maxime Thirouin <m@moox.io>
2018-06-13 18:29:37 +0200

    Enhance css inlined in head example to avoid same file on the fs multiple times

--- examples/react-app-styles-with-css-inlined-in-head/Html.js
+++ examples/react-app-styles-with-css-inlined-in-head/Html.js
@@ -1,33 +1,39 @@
 // @flow
 
+import fs from "fs";
+
 import * as React from "react";
+
+// memory to avoid reading a css file already read
+// @todo try to get ./dist/ from config somehow
+const stylesMemory = {};
+const readCssFile = css => {
+  if (!stylesMemory[css])
+    stylesMemory[css] = fs.readFileSync("./dist/" + css, { encoding: "utf8" });
+  return stylesMemory[css];
+};
 
 export default ({ App, render }: PhenomicHtmlPropsType) => {
   const { html, Main, State, Script, Style, assets } = render(<App />);
 
-  const styles =
-    process.env.PHENOMIC_ENV !== "static" ? (
-      <Style />
-    ) : (
-      <style
-        // @idea use https://github.com/addyosmani/critical
-        dangerouslySetInnerHTML={{
-          __html: Object.keys(assets)
-            .reduce((acc, name) => acc.concat(assets[name]), [])
-            .filter(asset => asset.endsWith(".css"))
-            .map(css =>
-              // @todo try to get /dist/ from config somehow
-              require("fs").readFileSync("./dist/" + css, { encoding: "utf8" })
-            )
-            .join("")
-            .replace(/\n/, "")
-        }}
-      />
-    );
-
   return (
     <html>
-      <head>{styles}</head>
+      <head>
+        {process.env.PHENOMIC_ENV !== "static" ? (
+          <Style />
+        ) : (
+          <style
+            // @idea use https://github.com/addyosmani/critical
+            dangerouslySetInnerHTML={{
+              __html: Object.keys(assets)
+                .reduce((acc, name) => acc.concat(assets[name]), [])
+                .filter(asset => asset.endsWith(".css"))
+                .map(css => readCssFile(css))
+                .join("")
+            }}
+          />
+        )}
+      </head>
       <body>
         <Main />
         <State />
