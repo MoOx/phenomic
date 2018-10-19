3a20dacf0b4d24f6072bd3d069fe42b40af2432a
Maxime Thirouin <git@moox.io>
2018-10-19 22:55:15 +0200

    Fix react-native-web usage/tests

--- examples/react-native-web-app/__tests__/index.js
+++ examples/react-native-web-app/__tests__/index.js
@@ -17,18 +17,22 @@ it("should build example correctly", () => {
   expect(file1).toContain("Hello World!");
   expect(file1).toContain("font-size:1.5em");
   expect(file1).toContain("text-align:center");
-  expect(file1).toContain("color:palevioletred");
+  // expect(file1).toContain("color:palevioletred");
+  expect(file1).toContain("color:rgba(219,112,147,1.00)");
   expect(file1).not.toContain("Hello again!");
   expect(file1).not.toContain("font-size:2em");
   expect(file1).not.toContain("text-align:right");
-  expect(file1).not.toContain("color:blue");
+  // expect(file1).not.toContain("color:blue");
+  expect(file1).not.toContain("color:rgba(0,0,255,1.00)");
 
   expect(file2).not.toContain("Hello World!");
   expect(file2).not.toContain("font-size:1.5em");
   expect(file2).not.toContain("text-align:center");
-  expect(file2).not.toContain("color:palevioletred");
+  // expect(file2).not.toContain("color:palevioletred");
+  expect(file2).not.toContain("color:rgba(219,112,147,1.00)");
   expect(file2).toContain("Hello again!");
   expect(file2).toContain("font-size:2em");
   expect(file2).toContain("text-align:right");
-  expect(file2).toContain("color:blue");
+  // expect(file2).toContain("color:blue");
+  expect(file2).toContain("color:rgba(0,0,255,1.00)");
 });
